3441151b76a06e3d1c0117e2b6fe0e8b5ce8238f
delila-pdf <delila.lenz@yahoo.de>
2020-01-29 00:47:05 +0100

    Revert DnD to circle-box

--- app.js
+++ app.js
@@ -13,19 +13,28 @@ let circleBox = document.querySelector("#circle-box");
 const gameStates = {
     initial: "start",
     listening: "listening",
-    ordering: "ordering"
+    ordering: "ordering",
+    checkorder: "checkorder"
 };
 let gameState = gameStates.initial;
 //beginning gameState
 
 const modal = document.querySelector("#modal");
+const modalText = document.querySelector("#modaltext");
 
 function hideModal() {
-    modal.style.opacity = "0";
     setTimeout(() => {
         modal.style.display = "none";
+        modalText.innerHTML = "";
     }, 300);
     modal.style.opacity = "0";
+}
+
+function displayModal(html) {
+    modal.style.display = "block";
+    modalText.innerHTML = html;
+    setTimeout(() => modal.style.opacity = "1", 0);
+
 }
 
 modal.addEventListener("click", function () {
@@ -33,7 +42,8 @@ modal.addEventListener("click", function () {
         case gameStates.initial:
             changeGameState(gameStates.listening);
             break;
-        case gameStates.listening:
+        case gameStates.ordering:
+            orderPhase();
             break;
         case gameStates.listening:
             break;
@@ -43,6 +53,7 @@ modal.addEventListener("click", function () {
 });
 
 function changeGameState(state) {
+    let prevState = gameState;
     gameState = state;
     switch (gameState) {
         case gameStates.initial:
@@ -53,14 +64,16 @@ function changeGameState(state) {
             startTimer(1000, 10000, gameStates.ordering);
             break;
         case gameStates.ordering:
-            startOrderPhase();
+            document.querySelectorAll(".circle").forEach(c => c.style.transform = "scale(0)")
+            displayModal(`<p> Now drag and drop the circles in the fitting order on the numbers</p>`);
+            document.querySelector("#progress").style.width = "100%";
+            document.querySelector("#sort-bar").style.display = "flex";
             break;
         default:
             break;
     }
 }
 //after clicking on modal
-
 
 
 function initSong(songNumber, colorSchemeNumber) {
@@ -74,7 +87,7 @@ function initSong(songNumber, colorSchemeNumber) {
     createSnippets(songNumber, colorSchemeNumber);
 
     const bar = document.querySelector("#progress-bar");
-    bar.addEventListener("click", startOrderPhase)
+    bar.addEventListener("click", orderPhase)
 }
 
 function createSnippets(songNumber, colorSchemeNumber) {
@@ -110,11 +123,9 @@ function buildCircle(circle, color, boxContainer, snippet) {
     circle.setAttribute("class", "circle");
     circle.setAttribute("style", "background:" + color);
 
-    setCirclePositionAndSize(circle, createRandomPositionAndSize(boxContainer));
+    setCircleSize(circle, createRandomDiameter(boxContainer));
 
-    circle.addEventListener("click", bounce)
-
-    function bounce() {
+    circle.addEventListener("click", function bounce() {
         if (!snippet.isPlaying) {
             // stop playing audio
             for (let i = 0; i < snippets.length; i++) {
@@ -128,38 +139,33 @@ function buildCircle(circle, color, boxContainer, snippet) {
             circle.style.animationDuration = "0.5s";
             circle.style.animationIterationCount = "infinite";
         }
-    };
+    });
     boxContainer.append(circle);
     setTimeout(() => { circle.style.transform = "scale(1)"; }, Math.random() * 300);
 }
 
 function buildAudio(snippet) {
-    snippet.audio.addEventListener("ended", stopBouncingandPlaying)
-
-    function stopBouncingandPlaying() {
+    snippet.audio.addEventListener("ended", function stopBouncingandPlaying() {
         snippet.isPlaying = false;
         snippet.circle.style.animationName = "none";
-    };
+    });
     snippet.audio.setAttribute("src", snippet.path);
     snippet.audio.setAttribute("class", "audio");
 }
 
-function setCirclePositionAndSize(circle, positionAndSize) {
-    //circle.style.left = positionAndSize.x + "px";
-    //circle.style.top = positionAndSize.y + "px";
-    circle.style.height = positionAndSize.durchmesser + "px";
-    circle.style.width = positionAndSize.durchmesser + "px";
+function setCircleSize(circle, diameter) {
+    circle.style.height = diameter + "px";
+    circle.style.width = diameter + "px";
 }
 
-function createRandomPositionAndSize(boxContainer) {
+function createRandomDiameter(boxContainer) {
     const bounding = boxContainer.getBoundingClientRect();
     // random durchmesser
-    let durchmesser = Math.random() * bounding.width;
-    while (durchmesser < bounding.width * 0.4) {
-        durchmesser = Math.random() * bounding.width;
+    let diameter = Math.random() * bounding.width;
+    while (diameter < bounding.width * 0.4) {
+        diameter = Math.random() * bounding.width;
     }
-
-    return { durchmesser: durchmesser };
+    return diameter;
 }
 
 function startTimer(initTime, alertTime, targetState) {
@@ -180,28 +186,31 @@ function startTimer(initTime, alertTime, targetState) {
     }, 1000);
 }
 
-function startOrderPhase() {
+
+
+function orderPhase() {
+    hideModal();
+    document.querySelectorAll(".circle").forEach(c => c.style.transform = "scale(1)");
     let rowLeft = document.querySelector("#row-left");
     let rowRight = document.querySelector("#row-right");
     let circles = document.querySelectorAll(".circle");
 
-    circleBox.style.flexDirection = "column";
-    circleBox.style.flexWrap = "nowrap";
+    // change to new circle layout
 
-    rowLeft.style.height = "60%";
-    rowLeft.style.width = "100%";
-    // 3 circles, each 20%
+    circleBox.classList.add("dice");
 
-    rowRight.style.height = "40%";
-    rowRight.style.width = "100%";
-    // 2 circles, each 20%
+    circles.forEach(c => circleBox.appendChild(c));
+    circleBox.removeChild(rowLeft);
+    circleBox.removeChild(rowRight);
 
     circles.forEach(c => {
         c.style.width = "25vw";
         c.style.height = "25vw";
     });
 
+
     // add top sort bar
+
 
     circles.forEach(c => {
         c.style.transition = "none";
@@ -216,10 +225,11 @@ function startOrderPhase() {
         console.log(e);
     });
 
-    let targetSortContainer; 
+    let targetSortContainer;
+    let sortBarContainers = document.querySelectorAll(".sort-container");
+
 
     draggable.on("drag:move", e => {
-
         if (e.data.sensorEvent.data.target.getAttribute("class") == "sort-container") {
             targetSortContainer = e.data.sensorEvent.data.target;
             e.data.sensorEvent.data.target.append(draggable.mirror);
@@ -230,13 +240,24 @@ function startOrderPhase() {
     });
 
     draggable.on("drag:stop", e => {
+        let circle = e.data.source;
         if (targetSortContainer != null) {
-            let circle = e.data.source;
             circle.style += "z-index: -1;";
             targetSortContainer.append(circle);
+
+            if ([...sortBarContainers].filter(e => e.childElementCount == 0).length == 0) {
+                console.log("All in position");
+                // diplayVerify()
+
+            } else {
+                console.log("Still ordering");
+                // hideVerify
+            }
+        } else if (circle.parentNode.getAttribute("class") == "sort-container") {
+            circleBox.append(circle);
         }
     });
 
-
+    startTimer(30000, 5000, gameStates.checkorder);
 
 }
