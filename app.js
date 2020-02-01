ff701e0ba2dba1ea6cc3afa99b46aa3c59d84c65
delila-pdf <delila.lenz@yahoo.de>
2020-02-01 21:20:21 +0100

    playable

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const snippetCount = 5;
 // snippet objects of a song
 let snippets = [];
+let songs = [{
+    title: "Don't Stop The Music",
+    artist: "Rihanna",
+    coverSrc: "./songs/song_0/cover.png"
+},
+{
+    title: "Maria Maria",
+    artist: "Santana",
+    coverSrc: "./songs/song_1/cover.jpg"
+}];
 // randomize snippets each time
 let colorSchemes = [
     ["#FFC19B", "#D3FF11", "#FF731E", "#04C7FF", "#D536FF"],
     ["#c2c1c5", "#a00068", "#26f4ff", "#0057FF", "#ff8cf1"],
-    ["FF3B01", "316CF4", "FF5FDB", "000000", "FDDFE1"]
+    ["#FF3B01", "#316CF4", "#FF5FDB", "#fff", "#FDDFE1"]
 ];
 let containerNumbers = [0, 1, 2, 3, 4];
 let circleBox = document.querySelector("#circle-box");
@@ -14,13 +24,23 @@ const gameStates = {
     initial: "start",
     listening: "listening",
     ordering: "ordering",
-    checkorder: "checkorder"
+    evaluate: "evaluate",
+    songpresentation: "songpresentation"
 };
-let gameState = gameStates.initial;
+let gameState;
+let currentSong = 0;
+let draggable;
+
 //beginning gameState
 
 const modal = document.querySelector("#modal");
 const modalText = document.querySelector("#modaltext");
+const infoBox = document.querySelector("#info-box");
+let progressBar = document.querySelector("#progress-bar");
+let skipIcon = document.querySelector("#skip-icon");
+const skipInfo = document.querySelector("#next-song");
+
+changeGameState(gameStates.initial);
 
 function hideModal() {
     setTimeout(() => {
@@ -38,12 +58,14 @@ function displayModal(html) {
 }
 
 modal.addEventListener("click", function () {
-    openFullscreen();
+    // openFullscreen();
     switch (gameState) {
         case gameStates.initial:
             changeGameState(gameStates.listening);
             break;
         case gameStates.ordering:
+            skipIcon.removeEventListener("click", resetProgressBarToOrdering)
+            skipIcon.addEventListener("click", resetProgressBarToEvaluate);
             orderPhase();
             break;
         case gameStates.listening:
@@ -53,22 +75,43 @@ modal.addEventListener("click", function () {
     }
 });
 
+function resetProgressBarToEvaluate() {
+    resetProgressBar(gameStates.evaluate)
+}
+
+function resetProgressBarToOrdering() {
+    resetProgressBar(gameStates.ordering)
+}
+
 function changeGameState(state) {
     let prevState = gameState;
     gameState = state;
     switch (gameState) {
         case gameStates.initial:
+            displayModal(`<p>Guess the right order of the song.</p>
+            <p> Click here to start! </p>`);
             break;
         case gameStates.listening:
+            skipIcon.addEventListener("click", resetProgressBarToOrdering);
             hideModal();
-            initSong(0, 0);
-            startTimer(1000, 10000, gameStates.ordering);
+            initSong(currentSong, currentSong);
+            startTimer(90000, 10000, gameStates.ordering);
             break;
         case gameStates.ordering:
-            document.querySelectorAll(".circle").forEach(c => c.style.transform = "scale(0)")
+            for (let i = 0; i < snippets.length; i++) {
+                snippets[i].audio.load();
+                snippets[i].isPlaying = false;
+                snippets[i].circle.style.animationName = "none";
+            };
+            document.querySelectorAll(".circle").forEach(c => {
+                c.style.transform = "scale(0)";
+            });
             displayModal(`<p> Now drag and drop the circles in the fitting order on the numbers</p>`);
             document.querySelector("#progress").style.width = "100%";
             document.querySelector("#sort-bar").style.display = "flex";
+            break;
+        case gameStates.evaluate:
+            evaluate();
             break;
         default:
             break;
@@ -80,15 +123,13 @@ function changeGameState(state) {
 function initSong(songNumber, colorSchemeNumber) {
     // remove old song
     snippets = [];
+
     // randomize containers of circles
     containerNumbers.sort(() => {
         return Math.random() >= 0.5 ? 1 : -1;
     });
 
     createSnippets(songNumber, colorSchemeNumber);
-
-    const bar = document.querySelector("#progress-bar");
-    bar.addEventListener("click", orderPhase)
 }
 
 function createSnippets(songNumber, colorSchemeNumber) {
@@ -121,16 +162,21 @@ function createSnippets(songNumber, colorSchemeNumber) {
 }
 
 function buildCircle(circle, color, boxContainer, snippet) {
+
     circle.setAttribute("class", "circle");
     circle.setAttribute("style", "background:" + color);
 
     setCircleSize(circle, createRandomDiameter(boxContainer));
 
-    circle.addEventListener("click", function bounce() {
+    circle.addEventListener("click", bounce);
+
+    boxContainer.append(circle);
+    setTimeout(() => { circle.style.transform = "scale(1)"; }, Math.random() * 300);
+    function bounce() {
         if (!snippet.isPlaying) {
             // stop playing audio
             for (let i = 0; i < snippets.length; i++) {
-                snippets[i].audio.load()
+                snippets[i].audio.load();
                 snippets[i].isPlaying = false;
                 snippets[i].circle.style.animationName = "none";
             };
@@ -140,10 +186,12 @@ function buildCircle(circle, color, boxContainer, snippet) {
             circle.style.animationDuration = "0.5s";
             circle.style.animationIterationCount = "infinite";
         }
-    });
-    boxContainer.append(circle);
-    setTimeout(() => { circle.style.transform = "scale(1)"; }, Math.random() * 300);
+    }
 }
+
+
+
+
 
 function buildAudio(snippet) {
     snippet.audio.addEventListener("ended", function stopBouncingandPlaying() {
@@ -169,10 +217,12 @@ function createRandomDiameter(boxContainer) {
     return diameter;
 }
 
+let remainingTime;
+let interval;
 function startTimer(initTime, alertTime, targetState) {
     document.querySelector("#progress").style.width = "100%";
-    let remainingTime = initTime;
-    var interval = setInterval(() => {
+    remainingTime = initTime;
+    interval = setInterval(() => {
         remainingTime -= 1000;
         document.querySelector("#progress").style.width = (remainingTime / initTime) * 100 + "%";
         if (remainingTime === alertTime - 1000) {
@@ -207,10 +257,10 @@ function orderPhase() {
         c.style.height = "25vw";
     });
 
-
     // add top sort bar
 
     circles.forEach((c, i) => {
+
         c.style.transition = "none";
         c.style.position = "fixed";
         c.style.top = "0";
@@ -236,7 +286,7 @@ function orderPhase() {
         }
     });
 
-    const draggable = new Draggable.Draggable(document.querySelectorAll("#game"), {
+    draggable = new Draggable.Draggable(document.querySelectorAll("#game"), {
         draggable: ".circle",
         delay: 0
     });
@@ -247,9 +297,10 @@ function orderPhase() {
     draggable.on("drag:move", e => {
         // draggable.js throws useless TypeErrors if you move out of the screen, thus we catch and ignore them
         try {
-            if (e.data.sensorEvent.data.target.getAttribute("class") == "sort-container") {
-                targetSortContainer = e.data.sensorEvent.data.target;
-                e.data.sensorEvent.data.target.append(draggable.mirror);
+            let target = e.data.sensorEvent.data.target;
+            if (target.getAttribute("class") == "sort-container") {
+                targetSortContainer = target;
+                target.append(draggable.mirror);
             } else {
                 circleBox.append(draggable.mirror);
                 targetSortContainer = null;
@@ -264,14 +315,6 @@ function orderPhase() {
         if (targetSortContainer != null) {
             circle.style += "z-index: -1;";
             targetSortContainer.append(circle);
-            // querySelectorAll returns a NodeList, filter() can only be called on Array -> transform with [...NodeList] (Rest Operator)
-            if ([...sortBarContainers].filter(e => e.childElementCount == 0).length == 0) {
-                console.log("All in position");
-                // diplayVerify()
-            } else {
-                console.log("Still ordering");
-                // hideVerify()
-            }
         } else {
             circleBox.append(circle);
             // use background as key (draggable.js somehow destroys the source/originalSource elements)
@@ -281,9 +324,150 @@ function orderPhase() {
         }
     });
 
-    startTimer(30000, 5000, gameStates.checkorder);
+    startTimer(30000, 5000, gameStates.evaluate);
 
 }
+
+
+function evaluate() {
+    skipIcon.removeEventListener("click", resetProgressBarToEvaluate);
+    clearInterval(interval);
+    document.querySelector("#progress").style.width = "0px";
+    try {
+        if (verifyOrder()) {
+            console.log("correct");
+            let song = songs[currentSong];
+            displayInfoBox(renderSong(song.title, song.artist, song.coverSrc));
+            let currentSnippet = 0;
+            snippets.forEach(s => {
+                s.audio.addEventListener("ended", () => {
+                    if (++currentSnippet > 4) return;
+                    let snippet = snippets[currentSnippet];
+                    let circle = snippet.circle;
+                    snippet.audio.play();
+                    snippet.isPlaying = true;
+                    circle.style.animationName = "bounce";
+                    circle.style.animationDuration = "0.5s";
+                    circle.style.animationIterationCount = "infinite";
+
+                });
+            });
+            snippets[0].audio.play();
+            snippets[0].isPlaying = true;
+            snippets[0].circle.style.animationName = "bounce";
+            snippets[0].circle.style.animationDuration = "0.5s";
+            snippets[0].circle.style.animationIterationCount = "infinite";
+
+            skipInfo.innerHTML = "next song";
+            skipInfo.style.opacity = 1;
+
+            skipIcon.addEventListener("click", nextSong);
+
+        } else {
+            displayInfoBox("FAIL");
+            skipInfo.innerHTML = "try again";
+            skipInfo.style.opacity = 1;
+            skipIcon.addEventListener("click", nextSong);
+        }
+    } catch (e) {
+        console.log("nothing to evaluate");
+        displayInfoBox("Time is up :(");
+        snippets.map(s => s.circle).forEach(c => c.style.transform = "scale(0)")
+        skipInfo.innerHTML = "try again";
+        skipInfo.style.opacity = 1;
+        skipIcon.addEventListener("click", nextSong);
+    }
+
+    function renderSong(title, artist, coverSrc) {
+        return `
+        <div class="song-meta">
+            <div class="correct">Nice job!</div>
+            <br>
+            <div class="tilte">${title}</div>
+            <div class="artist">${artist}</div>
+            <div class="cover">
+                <img src="${coverSrc}"/>
+            </div>
+        </div>`;
+    }
+
+}
+
+function verifyOrder() {
+    let correct = true;
+    let containers = document.querySelectorAll(".sort-container");
+    try {
+        for (let i = 0; i < 5; i++) {
+            if (containers[i].children[0].style.background != snippets[i].circle.style.background) {
+                correct = false;
+            }
+        }
+    } catch(e) {
+        return false;
+    }
+    
+    return correct;
+}
+
+function nextSong() {
+    if (verifyOrder()) currentSong++;
+    skipIcon.removeEventListener("click", nextSong);
+    draggable.destroy();
+    for (let i = 0; i < snippets.length; i++) {
+        snippets[i].audio.load();
+        snippets[i].isPlaying = false;
+        snippets[i].circle.style.animationName = "none";
+    };
+    circleBox.classList.remove("dice");
+    document.querySelector("#sort-bar").style.display = "none";
+
+    let leftRow = document.createElement("div");
+    leftRow.setAttribute("id", "row-left");
+    [document.createElement("div"), document.createElement("div"), document.createElement("div")]
+        .forEach(b => {
+            b.setAttribute("class", "box-left box-container");
+            leftRow.appendChild(b);
+        });
+
+    let rightRow = document.createElement("div");
+    rightRow.setAttribute("id", "row-right");
+    [document.createElement("div"), document.createElement("div")].forEach(b => {
+        b.setAttribute("class", "box-right box-container");
+        rightRow.appendChild(b);
+    });
+
+    circleBox.appendChild(leftRow);
+    circleBox.appendChild(rightRow);
+
+    document.querySelectorAll(".circle").forEach(c => c.parentNode.removeChild(c));
+    hideInfoBox();
+    skipInfo.style.opacity = 0;
+    changeGameState(gameStates.initial);
+}
+
+
+
+
+function clearEventListeners(node) {
+    return node.parentNode.replaceChild(node.cloneNode(true), node);
+}
+
+function displayInfoBox(html) {
+    infoBox.innerHTML = html;
+    infoBox.style.opacity = 1;
+}
+
+function hideInfoBox() {
+    infoBox.style.opacity = 0;
+}
+
+function resetProgressBar(targetState) {
+    clearInterval(interval);
+    changeGameState(targetState);
+}
+
+
+
 
 // FULLSCREEN FROM https://www.w3schools.com/jsref/met_element_requestfullscreen.asp
 /* Get the documentElement (<html>) to display the page in fullscreen */
@@ -299,6 +483,8 @@ function openFullscreen() {
         elem.webkitRequestFullscreen();
     } else if (elem.msRequestFullscreen) { /* IE/Edge */
         elem.msRequestFullscreen();
+    } else {
+
     }
 }
 
